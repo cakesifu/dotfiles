86d86bf6b269d33ab2713ba667dcc8a0dd7e280e
Cezar Berea <berea.cezar@gmail.com>
2017-04-11 13:38:07 +0300

    Tweak colordebug.sh

--- bin/bin/colordebug.sh
+++ bin/bin/colordebug.sh
@@ -3,10 +3,15 @@
 
 echo "regular "
 
-for x in {0..3}; do
+values=(0 1 7)
+
+for x in ${values[@]}; do
     for i in {30..37}; do
             echo -ne "\e[$x;$i""m\\\e[$x;$i""m\e[0;37 "
+            echo -ne '\e[0m'
         echo
     done
 done
-echo ""
+
+echo -ne '\e[0m'
+echo "done"
