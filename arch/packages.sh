1de23719b4188ecf84c67b8e18c2efedd2963f08
Cezar Berea <cezar@dotts.ro>
2024-05-21 14:42:05 +0300

    Update arch p ackages install script

--- arch/packages.sh
+++ arch/packages.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy \
+pacman -Sy \
 # echo \
-  git vim sudo neovim helix openssh chezmoi starship `# console` \
-  alacritty firefox thunar `# gui basics` \
+  git vim sudo neovim helix openssh chezmoi starship curl wget tree yq `# console` \
+  alacritty firefox thunar oculante`# gui basics` \
+  greetd greetd-tuigreet `# login` \
   hyprland xdg-desktop-portal-hyprland wdisplays hyprpaper waybar waylock wlogout rofi-wayland dunst swayidle wl-clipboard `# DE` \
   pipewire pipewire-audio pavucontrol `# audio` \
   ttf-firacode-nerd ttf-ubuntu-nerd ttf-ubuntu-mono-nerd ttf-jetbrains-mono-nerd ttf-sourcecodepro-nerd `# fonts` 
