b351680e54e1f92a00e148a6996382184209afb3
Cezar Berea <cezar@dotts.ro>
2024-05-22 17:55:23 +0300

    Update arch packages

--- arch/packages.sh
+++ arch/packages.sh
@@ -2,9 +2,10 @@
 
 pacman -Sy \
 # echo \
-  git vim sudo neovim helix openssh chezmoi starship curl wget tree yq `# console` \
+  bash-completion git vim sudo neovim helix openssh chezmoi starship curl wget tree yq `# console` \
   alacritty firefox thunar oculante`# gui basics` \
   greetd greetd-tuigreet `# login` \
   hyprland xdg-desktop-portal-hyprland wdisplays hyprpaper waybar waylock wlogout rofi-wayland dunst swayidle wl-clipboard `# DE` \
   pipewire pipewire-audio pavucontrol `# audio` \
-  ttf-firacode-nerd ttf-ubuntu-nerd ttf-ubuntu-mono-nerd ttf-jetbrains-mono-nerd ttf-sourcecodepro-nerd `# fonts` 
+  ttf-firacode-nerd ttf-ubuntu-nerd ttf-ubuntu-mono-nerd ttf-jetbrains-mono-nerd ttf-sourcecodepro-nerd `# fonts` \
+  taplo-cli bash-language-server yaml-language-server typescript-language-server vscode-json-languageserver vscode-css-languageserver
