1d19633dada62c7005f3ec93b24ec4d0b09d054d
Cezar Berea <cezar@dotts.ro>
2024-05-27 16:57:04 +0300

    Update hyprland colors and add vim \cp key

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -4,7 +4,12 @@
 local map = vim.keymap.set
 local unmap = vim.keymap.del
 
+local copy_path = function()
+  local filepath = vim.fn.expand("%")
+  vim.fn.setreg("+", filepath)
+end
 -- yank/past to/from global register
 map("n", "\\y", '"+y', { desc = "yank to + register" })
 map("n", "\\p", '"+p', { desc = "paste (after cursor) from + register" })
 map("n", "\\P", '"+P', { desc = "paste (before cursor) from + register" })
+map("n", "\\cp", copy_path, { desc = "copy file path to clipboard" })
