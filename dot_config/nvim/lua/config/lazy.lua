dd4e185f0aebb12be2351c7a61d02655063d430d
Cezar Berea <cezar@dotts.ro>
2024-10-16 15:27:08 +0300

    Add svelte support in nvim

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -18,9 +18,12 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.util.chezmoi" },
-    { import = "lazyvim.plugins.extras.lang.nushell" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
+    -- dap
+    { import = "lazyvim.plugins.extras.dap.core" },
+
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -15,7 +15,7 @@ return {
         javascript = { "prettier" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
-        rust = { "rustfmt", lsp_format = "fallback" },
+        rust = { "rustfmt", lsp_format = "first" },
       },
       formatters = {
         prettier = {
@@ -32,7 +32,6 @@ return {
         follow_current_file = {
           enabled = false,
         },
-        bind_to_cwd = true,
       },
     },
   },
